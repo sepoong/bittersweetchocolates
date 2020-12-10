08c7a623c9ce2663769024ecd32c96c724df77b1
sepoong <74528973+sepoong@users.noreply.github.com>
2020-12-11 01:12:23 +0900

    Update inter.c

--- exam/ex02/inter.c
+++ exam/ex02/inter.c
@@ -12,35 +12,6 @@
 
 #include <unistd.h>
 
-//int main(int ac, char **av)
-//{
-//	int i;
-//	int j;
-//	char *temp;
-//
-//	i = 0;
-//	temp = 0;
-//	if (ac == 3)
-//	{
-//		while (av[1][i])
-//		{
-//			j = 0;
-//			while (av[2][j])
-//			{
-//				if (av[1][i] == av[2][j])
-//					*temp = av[1][i];
-//					write(1, temp, 1);
-//					j++;
-//			}
-//			i++;
-//		}
-//		write(1, "\n", 1);
-//	}
-//	return (0);
-//}
-
-
-
 void inter(char *str1, char *str2);
 
 int main(int argc, char** argv)
@@ -59,15 +30,15 @@ void inter(char *str1, char *str2)
 
 	j = 0;
 	while (j < 256)
-		check[j++] = 0; // 모든 배열을 0으로 초기.:
+		check[j++] = 0; // 모든 배열을 0으로 초기화, j로 돌림
 	i = 0;
 
 	while (str1[i] != 0)
 	{
 		j = 0;
-		while (str2[j] != 0)
+		while (str2[j] != 0)   // i, 다음 j로 !!
 		{
-			if (str1[i] == str2[j] && check[(int)str1[i]] == 0)
+			if (str1[i] == str2[j] && check[(int)str1[i]] == 0)  // i로 돌린다
 			{   
 				check[(int)str1[i]] = 1; // 일로 세팅되기 때문에 다시 같은 수가반복
 				write(1, &str1[i], 1);  //되면 조건 값에 의해 패스, str1에다 하는거

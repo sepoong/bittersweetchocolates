f155d553052186d586a7537f0bbbb92c93b76fac
sepoong <74528973+sepoong@users.noreply.github.com>
2020-12-11 00:18:48 +0900

    Update ft_atoi.c

--- exam/ex02/ft_atoi.c
+++ exam/ex02/ft_atoi.c
@@ -10,6 +10,38 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h> //
+
+int		ft_atoi(char *s)
+{
+	int		sign;
+	long	r;
+
+	r = 0;
+	sign = 1;
+	while (*s == 32 || (*s >= 9 && *s <= 13))
+		s++;
+	if (*s == '-' || *s == '+')
+	{
+		if (*s == '-')
+			sign = -1;
+		s++;
+	}
+	while (*s >= '0' && *s <= '9')
+	{
+		r = r * 10 + *s - '0';
+		s++;
+	}
+	return (sign * (int)r);
+}
+
+int		main(void)//
+{//
+	char a[] = "    \n\n\v\f\r\t -5234AAAgreghrsth";// -5234
+	printf("%d\n",ft_atoi(a));//
+}//
+
+
 #include <stdio.h>
 
 int	ft_atoi(const char *str)

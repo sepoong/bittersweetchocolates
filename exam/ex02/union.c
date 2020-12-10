cadef56b34a4b1772aab414df91f0b372008bc97
sepoong <74528973+sepoong@users.noreply.github.com>
2020-12-11 03:09:56 +0900

    Update union.c

--- exam/ex02/union.c
+++ exam/ex02/union.c
@@ -35,15 +35,15 @@ $>
 
 #include <unistd.h>
 
-int		not_seen_before(char *s, int max_pos, char c)
+int		not_seen_before(char *s, int i, char c)
 {
-	int i;
+	int n;
 
-	i = 0;
-	while (i < max_pos)
-		if (s[i] == c)
+	n = 0;
+	while (n < i)  //그전에 i = n 일때까지 같은 문자가 나오면 안됨
+		if (s[n] == c)
 			return (0);
-	 	i++;
+	 	n++;
 	return (1);
 }
 
@@ -54,7 +54,7 @@ void	ft_union(char *s1, char *s2)
 
 	i = 0 ;
 	while (s1[i])
-		if (not_seen_before(s1, i, s1[i])) // 즉, 리턴이 1인 경우..
+		if (not_seen_before(s1, i, s1[i])) // 즉, 리턴이 1인 경우.. 3가지 매개변수
 			write(1, &s1[i], 1);
 			i++;
 	j = 0;

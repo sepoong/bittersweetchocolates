d026875539010c2cef5b60e6b3f11583fb11d07b
sepoong <74528973+sepoong@users.noreply.github.com>
2020-12-10 23:01:08 +0900

    Update ft_countdown.c

--- exam/ex00/ft_countdown.c
+++ exam/ex00/ft_countdown.c
@@ -25,3 +25,16 @@ int main()
 	write(1, "\n", 1);
 	return (0);
 }
+
+Assignment name  : ft_countdown
+Expected files   : ft_countdown.c
+Allowed functions: write
+--------------------------------------------------------------------------------
+
+Write a program that displays all digits in descending order, followed by a
+newline.
+
+Example:
+$> ./ft_countdown | cat -e
+9876543210$
+$>
